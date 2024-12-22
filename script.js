edc931b780eee8f50122232759409bd19cd2d28e
Yoda-Flash <kaitlyn.zee@gmail.com>
2024-12-21 17:32:09 -0800

    Finished site for now

---
+++ script.js
@@ -0,0 +1,23 @@
+const music = document.getElementById("music");
+// https://www.youtube.com/watch?v=STdKeu9pmGY
+const musicButton = document.getElementById("music_button");
+
+musicButton.addEventListener("click", () => {
+    music.play();
+    console.log(music);
+})
+
+const swoosh = document.getElementById("swoosh");
+const falcon = document.getElementById("falcon");
+
+falcon.addEventListener("animationiteration", startSwoosh);
+
+function startSwoosh() {
+    swoosh.play();
+    console.log("Swooshing")
+}
+
+// if (falcon.checkVisibility() && !music.paused) {
+//     swoosh.play();
+//     console.log(falcon.visibility);
+// }
